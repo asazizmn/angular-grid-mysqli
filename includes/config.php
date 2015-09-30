7ebbc9c90a68162cb3c5b9efb6f4e8c6aa1e1b0e
Aziz <azizsaboor@gmail.com>
2015-09-30 08:55:26 +0800

    - config update

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php 
-$DB_HOST = '127.0.0.1';
-$DB_USER = 'root';
+$DB_HOST = '';
+$DB_USER = '';
 $DB_PASS = '';
-$DB_NAME = 'angularcode_grid';
+$DB_NAME = '';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
